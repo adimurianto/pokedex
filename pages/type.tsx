2a4016837ec7cf6c32dda7eeda23c61a947337d1
AM Codes <amccodes@gmail.com>
2022-10-28 00:14:55 +0700

    Create mechanism get data types pokemon and create list types on page pokemon types

--- pages/type.tsx
+++ pages/type.tsx
@@ -1,20 +1,54 @@
+import axios from "axios";
+import { useState } from "react";
 import Layout from "../components/Layout";
+import styles from '../styles/Types.module.css';
 
-export default function Type(initialPokemon: any) {
-    return (
-      <Layout>
-        <h1>Comming Soon</h1>
-      </Layout>
-    )
+export default function Type(props: any) {
+  const [types, setTypes] = useState(props.initialPokemonTypes);
+  const typeSelected = types.results[0].name[0].toUpperCase() + types.results[0].name.substr(1);
+  const [typeSelect, setTypeSelect] = useState(typeSelected);
+  
+  return (
+    <Layout>
+      <div className={styles.body_types}>
+        <div className={styles.sidebar}>
+          <h4>Pokemon Type</h4>
+          <ul>
+            {
+              types.results.map((type: any, index: number) => (
+                <li 
+                  className={styles.list_type}
+                  key={index}
+                >
+                  {
+                    type.name[0].toUpperCase() + type.name.substr(1)
+                  }  
+                </li>
+              ))
+            }
+          </ul>
+        </div>
+
+        <div className={styles.contents}>
+            <h2>{`Pokemon with Type ${typeSelect}`}</h2>
+        </div>
+      </div>
+    </Layout>
+  )
 }
 
 export async function getStaticProps(context: any) {
-    const response = await fetch("https://pokeapi.co/api/v2/pokemon");
-    const initialPokemon = await response.json();
-
+  try {
+    const result = await axios.get("https://pokeapi.co/api/v2/type");
+    const initialPokemonTypes = result.data;
     return {
         props: {
-            initialPokemon
+          initialPokemonTypes
         }
     }
+  } catch (error) {
+    return {
+        props: {}
+    }
+  }
 }
